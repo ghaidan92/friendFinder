95ee27f238bb8fcd98f552122e0586cf9d648e6c
ghaidan <you@example.com>
2018-12-18 20:00:46 -0800

    mm

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,10 +4,10 @@ var friends = [
         name: "Ghaidan",
         photo: "https://avatars1.githubusercontent.com/u/43773435?s=460&v=4",
         score: [
-            2,
+            1,
+            5,
             3,
-            4,
-            4,
+            2,
             4,
             1,
             2,
